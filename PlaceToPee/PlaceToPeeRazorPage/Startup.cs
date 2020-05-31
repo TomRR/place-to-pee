fd75eb1326dda1e9d24c0c9a76cbb3401be800d4
TomRR <resingtr@gmail.com>
2020-05-31 18:44:58 +0200

    add connection string

--- PlaceToPee/PlaceToPeeRazorPage/Startup.cs
+++ PlaceToPee/PlaceToPeeRazorPage/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataLibrary.Data;
+using DataLibrary.Db;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +26,14 @@ namespace PlaceToPeeRazorPage
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSingleton(new ConnectionStringData
+            {
+                SqlConnectionName = "Default"
+            });
+
+            services.AddTransient<IDataAccess, SqlDb>();
+            services.AddTransient<IStandorteData, StandorteData>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
